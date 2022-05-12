ed1f385cfd45a549028c998344c92f2f00d1baf1
Gbemisola Kotoye <barkty@yahoo.com>
2022-05-12 14:44:57 +0100

    jewels n stones

--- hash-tables/practice-solutions/jewels-and-stones-barkty.js
+++ hash-tables/practice-solutions/jewels-and-stones-barkty.js
@@ -1,3 +1,30 @@
 var numJewelsInStones = function(jewels, stones) {
-    
+    //Set the counter
+    let counter = 0;
+
+    stones.split('').forEach(element => {
+        jewels.indexOf(element) >= 0 ? counter++ : counter
+    });
+
+    console.log(counter);
+
+
+    // ?//////////////////////////////////////
+    let stoneHash = {
+        substring1: numberOfOccurrences
+    };
+
+    const stone = stones.split('');
+
+    stone.forEach(element => {
+        if(!stoneHash[element] || stoneHash[element] === 0) {
+            stoneHash[element] = 1
+        }
+        stoneHash[element]++
+    });
+
+    console.log(stoneHash)
+    return counter;
 };
+
+numJewelsInStones('sss', 'seDftrssdetr');
