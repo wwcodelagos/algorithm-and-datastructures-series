5cfb2f3948a67606615c05b7606f9479813825d2
Tosin Rotimidokun <51387182+rotimidokun@users.noreply.github.com>
2022-06-11 21:02:19 +0100

    Solved Plus One problem in Javascript

---
+++ strings-and-arrays/practice-solutions/plus-one-rotimidokun.js
@@ -0,0 +1,16 @@
+//Leetcode 66. 
+//Solved using Javascript.
+
+var plusOne = function(digits) {
+    //Join the items in the array. Remember to let the computer know, it is a type //of Number so it can be incremented by 1
+  const joinNumbers = Number(digits.join(""));
+
+    // Increment the result numbers by 1
+  const incrementNumber = joinNumbers + 1;
+  
+    // convert back to string, and split to an array of items. Split method work on     //strings.
+  const splitNumbers = incrementNumber.toString().split("");
+
+  // return result
+  return splitNumbers;
+};
